6f425a59f989419f417208b2de4162631730edf2
Vincent Cannalla <vincentcannalla@Vincents-MacBook-Pro-4.local>
2019-11-13 13:42:19 -0800

    updated python version for forever script

--- forever.py
+++ forever.py
@@ -5,7 +5,7 @@ import sys
 filename = sys.argv[1]
 while True:
     print("\nStarting " + filename)
-    p = Popen("python " + filename, shell=True)
+    p = Popen("python3 " + filename, shell=True)
     p.wait()
     if not p.returncode:
         break
