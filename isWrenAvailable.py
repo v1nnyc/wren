cb41f23a0c88d3ece8fc2d77046fddf817e29809
Vincent Cannalla <vincentcannalla@Vincents-MacBook-Pro-4.local>
2019-11-12 23:13:06 -0800

    updated selenium settings and email

--- isWrenAvailable.py
+++ isWrenAvailable.py
@@ -7,18 +7,21 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 
-import smtplib, ssl
+import smtplib
+import ssl
 
 port = 465  # For SSL
+
+pet_id_to_search = 633418
+
 
 # method to webscrabe humane society to see if wren has been put up for adoption
 def isWrenAvailable():
     # TODO replace with wrens ID
-    pet_id_to_search = 228652
 
     options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
+    # options.add_argument("--headless")
+    # options.add_argument("--disable-gpu")
 
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     driver.get(
@@ -36,14 +39,26 @@ def isWrenAvailable():
     soup = BeautifulSoup(content, features="html.parser")
     for div in soup.findAll("span", {"class": "petId"}):
         if div.next_element == " #{}".format(pet_id_to_search):
+            driver.stop_client()
+            driver.close()
             return True
+    driver.stop_client()
+    driver.close()
 
 
 def emailMe():
     print("emailing.. lol")
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=ssl.create_default_context()) as server:
         server.login("vcannall@gmail.com", "vtslgyxfmzqqewvq")
-        server.sendmail("vcannall@gmail.com", "vcannall@gmail.com", "WREN IS AVAILABLE")
+        server.sendmail(
+            "vcannall@gmail.com",
+            "vcannall@gmail.com",
+            """
+            https://www.sdhumane.org/adopt/available-pets/?petType=ALL&physicalLocationId=ALL&genderSearch=All&searchAge=All&searchName=&searchId={}
+            """.format(
+                pet_id_to_search
+            ),
+        )
 
 
 # main
